546ddc5255ff739299140a3dc10487d0b2bd898e
Mars1327 <148791029+Mars1327@users.noreply.github.com>
2023-10-29 15:46:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+
+
+namespace homeWorkC_2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Sinitsyn Anatoliy Aleksandrovich";
+            byte age = 40;
+            string email = "sin1094606@yandex.ru";
+            byte scoreProgram = 87;
+            byte scoreMathem = 92;
+            byte scorePhysics = 82;
+
+            string pattern = "ФИО: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5} ";
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              scoreProgram,
+                              scoreMathem,
+                              scorePhysics);
+
+            Console.ReadLine();
+
+            int sumAllScore = scoreProgram + scoreMathem + scorePhysics;
+            double averageScore = sumAllScore / 3;
+
+            Console.WriteLine($"Сумма баллов: {sumAllScore} \nСредний балл: {averageScore}");
+            
+            Console.ReadLine();
+
+        }
+
+    }
+}
